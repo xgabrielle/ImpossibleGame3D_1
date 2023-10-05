2d510ed921224dd4c5b4e00bbe5e2dd192dab657
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-05 16:13:13 +0200

    follow player script

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,8 @@ public class FollowPlayer : MonoBehaviour
 {
     private void LateUpdate()
     {
-        GameObject player = GameObject.Find()
+        GameObject player = GameObject.Find("Player");
+        Vector3 targetPosition = player.transform.position + new Vector3(-4, 2, 0);
+        transform.position = targetPosition;
     }
 }
