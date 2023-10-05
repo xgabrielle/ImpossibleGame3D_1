c9d502163a2e46afebf6795d50a4604108514ed0
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-05 16:03:51 +0200

    new script for camera

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        GameObject player = GameObject.Find()
+    }
+}
